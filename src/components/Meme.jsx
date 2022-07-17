cceae20728f5d97d62b6c72a5d561ca4daed9792
abhiXweb <dev.abhisharma@gmail.com>
2022-07-17 14:30:52 +0530

    added meme generator form

---
+++ src/components/Meme.jsx
@@ -0,0 +1,13 @@
+const Meme = () => {
+    return (
+        <main>
+            <form className="form">
+                    <input className="form--input" placeholder="Top Text" type="text" name="" id="" />
+                    <input className="form--input" placeholder="Bottom Text" type="text" name="" id="" />
+                    <input className="form--button" type="submit" value="Get a new meme image 🖼" name="" id="" />
+            </form>
+        </main>
+    )
+}
+
+export default Meme
