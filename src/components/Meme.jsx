11bb2807a04e0d4b90df341b86aa5dd716fcb81d
abhiXweb <dev.abhisharma@gmail.com>
2022-07-18 21:59:33 +0530

    getting random img url

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,15 @@
+import memeData from "../data/memeData";
 const Meme = () => {
+    const handleSubmit = ( form ) => {
+        form.preventDefault();
+        const memes = memeData.data.memes;
+        const randomNum = Math.floor(Math.random() * memes.length)
+        const url = memes[randomNum].url;
+        console.log(url);
+    }
     return (
         <main>
-            <form className="form">
+            <form onSubmit={handleSubmit} className="form">
                     <input className="form--input" placeholder="Top Text" type="text" name="" id="" />
                     <input className="form--input" placeholder="Bottom Text" type="text" name="" id="" />
                     <input className="form--button" type="submit" value="Get a new meme image 🖼" name="" id="" />
