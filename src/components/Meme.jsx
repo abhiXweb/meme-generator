eb0e324644eee5a92e5ed8dafc060008ac4eb871
abhiXweb <dev.abhisharma@gmail.com>
2022-07-21 22:36:47 +0530

    updated input field

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,22 @@
+import React from "react"
 import memeData from "../data/memeData";
 const Meme = () => {
-    const handleSubmit = ( form ) => {
+    let [url, setUrl] = React.useState("");
+    const handleSubmit = (form) => {
         form.preventDefault();
         const memes = memeData.data.memes;
         const randomNum = Math.floor(Math.random() * memes.length)
-        const url = memes[randomNum].url;
-        console.log(url);
+        url = memes[randomNum].url;
+        setUrl(url);
     }
     return (
         <main>
             <form onSubmit={handleSubmit} className="form">
-                    <input className="form--input" placeholder="Top Text" type="text" name="" id="" />
-                    <input className="form--input" placeholder="Bottom Text" type="text" name="" id="" />
-                    <input className="form--button" type="submit" value="Get a new meme image 🖼" name="" id="" />
+                <input className="form--input" placeholder="Top Text" type="text" name="" id="" />
+                <input className="form--input" placeholder="Bottom Text" type="text" name="" id="" />
+                <input className="form--button" type="submit" value="Get a new meme image 🖼" name="" id="" />
             </form>
+            <img src={url} alt="" />
         </main>
     )
 }
